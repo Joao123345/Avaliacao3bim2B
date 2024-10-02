06fb6ed6979f0340870bb738ec7087daf25909ce
joao <00001093323917SP@al.educacao.sp.gov.br>
2024-10-01 21:22:50 -0300

    joao

---
+++ j.py
@@ -0,0 +1,25 @@
+def
+
+candidato = {leo = "vereador"}
+número = [1]
+nome_do_partido = ['psb']
+print
+
+if
+
+candidato = [pedro = "governador"]
+número = [2]
+nome_do_partido = ['pt']
+print
+
+if (nome)
+
+candidato = [lucas = "vereador"]
+número = [3]
+nome_do_partido = ['pl']
+print
+
+#digite o nome do candidato
+input("digite o nome do candidato")
+
+branch
